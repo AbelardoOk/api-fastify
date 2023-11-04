2d3ce678b96a1aa32d309ad6aa80b886d7541b7d
AbelardoOk <Abelardoemailprivado@gmail.com>
2023-11-04 14:48:43 -0400

    feat: Created query parameters

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ server.post("/videos", (request, reply) => {
   return reply.status(201).send();
 });
 
-server.get("/videos", () => {
-  const videos = database.list();
+server.get("/videos", (request) => {
+  const search = request.query.search;
+  const videos = database.list(search);
 
   return videos;
 });
